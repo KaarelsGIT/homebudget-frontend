d18e14274adb4063c589039362f40ee5a56514a8
kaarel <kaarel.kattai@icloud.ee>
2025-06-14 22:30:44 +0300

    Added transaction

--- src/app/shared/transaction/transaction.service.ts.ts
+++ src/app/shared/transaction/transaction.service.ts.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environments';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionServiceTs {
+  private baseUrl = environment.apiURL;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  addTransaction(type: 'income' | 'expense', transaction: any): Observable<any> {}
 }
